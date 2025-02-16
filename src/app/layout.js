e700b99f742b1dd53f009048aea528f73973bb85
Aditya Tyagi <adityatyagiav1@gmail.com>
2025-02-16 19:21:24 +0530

    css issue fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,8 @@
 "use client";
-
-
-import "./globals.css";
+import  "bootstrap/dist/css/bootstrap.min.css"
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
-
+import './globals.css'
 import { useEffect, useState } from "react";
 import store from "@/redux/store";
 
@@ -21,7 +19,7 @@ export default function RootLayout({ children }) {
   }
   return (
     <html lang="en">
-      <body className={`antialiased`}>
+      <body >
         <Provider store={store}>
           <ToastContainer />
           {children}
